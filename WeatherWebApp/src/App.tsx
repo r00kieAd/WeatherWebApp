e0b2cb2b9e30d76173827a94aafe3d5b5dca9522
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2025-05-30 16:36:03 +0530

    added starting structure for the UI

--- WeatherWebApp/src/App.tsx
+++ WeatherWebApp/src/App.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="outerWrap">
+        <div id="mainConainer">
+          <div id="temperatureDiv">
+            <div id="temperature">
+              {/* need to add html for displaying temp stats */}
+            </div>
+            <div id="tempIcon">
+              {/* need to add html for displaying related weather icon */}
+            </div>
+          </div>
+          <div id="otherTempStats">
+            <div id="aqiStat">
+              {/* need to add html */}
+            </div>
+            <div id="humidityStat">
+              {/* need to add humidity stat html */}
+            </div>
+          </div>
+          <div id="futureDaysStats">
+            {/* need to add html structure */}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
