48e19e2488eaf6af8918a50276888f16a5b67016
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2025-05-31 19:31:52 +0530

    updated structure with css

--- WeatherWebApp/src/App.tsx
+++ WeatherWebApp/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import clearSky from './assets/clear-sky.png'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
       <div id="outerWrap">
-        <div id="mainConainer">
+        <div id="mainContainer">
           <div id="temperatureDiv">
-            <div id="temperature">
+            <div id="temperatureInfo">
               <div id="temperatureContainer">
-                <div className="main-temp-value temp-div">0</div>
-                <div className="high-temp-value temp-div">H: <span id="currHighTemp">+5</span></div>
-                <div className="low-temp-value temp-div">H: <span id="currLowTemp">-2</span></div>
+                <div className="main-temp-value temp-div"><span id="currTemp">0</span></div>
+                <div className="sub-temp-stats temp-div">
+                  <div className="high-temp-value sub-temp-div">H: <span id="currHighTemp">+5</span></div>
+                  <div className="low-temp-value sub-temp-div">H: <span id="currLowTemp">-2</span></div>
+                </div>
               </div>
             </div>
-            <div id="tempIcon">
+            <div id="temperatureIcon">
               <img className="weather-icon clear-sky" src={clearSky} alt="clear sky" />
             </div>
           </div>
           <div id="otherTempStats">
             <div id="aqiStat">
-              <div id="aqiContainer">
+              <div id="aqiContainer" className='other-stats-sub-div'>
                 <div className="aqi-text aqi-sub-div">Air Quality Index:</div>
                 <div className="aqi-value aqi-sub-div"><span id="aqi">Good</span></div>
               </div>
             </div>
             <div id="humidityStat">
-              <div id="humidityContainer">
+              <div id="humidityContainer" className='other-stats-sub-div'>
                 <div className="humidity-text humidity-sub-div">Humidity:</div>
                 <div className="humidity-value humidity-sub-div"><span id="humidity">24%</span></div>
               </div>
@@ -39,7 +41,25 @@ function App() {
             <div id="futureStatContainer">
               <div id="day1">
                 <div className="day-name future-stat-div">Today</div>
-                <div className="day-weather-icon future-stat-div"><img src={clearSky} alt="clear sky" /></div>
+                <div className="day-weather-icon future-stat-div"><img src="" alt="clear sky" /></div>
+                <div className="day-high future-stat-div">+26</div>
+                <div className="day-low future-stat-div">+22</div>
+              </div>
+              <div id="day2">
+                <div className="day-name future-stat-div">Today</div>
+                <div className="day-weather-icon future-stat-div"><img src="" alt="clear sky" /></div>
+                <div className="day-high future-stat-div">+26</div>
+                <div className="day-low future-stat-div">+22</div>
+              </div>
+              <div id="day3">
+                <div className="day-name future-stat-div">Today</div>
+                <div className="day-weather-icon future-stat-div"><img src="" alt="clear sky" /></div>
+                <div className="day-high future-stat-div">+26</div>
+                <div className="day-low future-stat-div">+22</div>
+              </div>
+              <div id="day4">
+                <div className="day-name future-stat-div">Today</div>
+                <div className="day-weather-icon future-stat-div"><img src="" alt="clear sky" /></div>
                 <div className="day-high future-stat-div">+26</div>
                 <div className="day-low future-stat-div">+22</div>
               </div>
